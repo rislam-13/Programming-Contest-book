b087883e308b023bb4f2bec356b74a6ffe4bb87b
Reazul Islam <40799648+rislam-13@users.noreply.github.com>
2018-09-15 00:19:40 +0600

    Create 4.cpp

---
+++ Chapter 2/Page 23 Excercise/4.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#define PI 3.1415926
+using namespace std;
+
+int main()
+{
+    double r, circumference;
+    cin>>r;
+
+    circumference=2*PI*r;
+
+    cout<<circumference<<"\n";
+
+    return 0;
+}
