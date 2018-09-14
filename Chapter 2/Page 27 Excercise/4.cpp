9a57fc58c869e1eabd893dbdba2c17f3c74073b4
Reazul Islam <40799648+rislam-13@users.noreply.github.com>
2018-09-15 01:29:40 +0600

    Create 4.cpp

---
+++ Chapter 2/Page 27 Excercise/4.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+    int ara[3];
+    cin>>ara[0]>>ara[1]>>ara[2];
+
+    int n=sizeof(ara)/sizeof(ara[0]);
+
+    sort(ara, ara+n);
+
+    cout<<ara[0]<<" "<<ara[1]<<" "<<ara[2]<<endl;
+
+    return 0;
+}
