91c8f743f61d265539d9e2945dd53c5831523694
Reazul Islam <40799648+rislam-13@users.noreply.github.com>
2018-10-22 23:59:43 +0600

    Create 4.cpp

---
+++ Chapter 2/Page 32 Excercise/4.cpp
@@ -0,0 +1,19 @@
+/*
+same as no. 1
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, sum;
+    cin>>n;
+
+    sum=(n*(n+1))/2;
+
+    cout<<sum<<endl;
+
+    return 0;
+}
